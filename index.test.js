00c976a52f886a3cd9e4e0a21be62f21967b8ff3
tstwroot <tstwroot@protonmail.com>
2023-07-07 22:03:56 -0400

    :bulb: [docs]: TODO comments

--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const database = require("./database");
 const app = require("./server");
 
+// TODO: make all tests
 describe("Write Tests", () => {
   it("Should make tests", async () => {
     expect(2 + 2).toEqual(4);
